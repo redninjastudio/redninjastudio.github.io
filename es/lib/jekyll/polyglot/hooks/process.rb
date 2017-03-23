9c34084fd9e3d5cc422ba2b4524e875fafa18bc2
franci <flikxxi@gmail.com>
2017-03-23 18:05:57 +0100

    clean

--- es/lib/jekyll/polyglot/hooks/process.rb
+++
@@ -1,7 +0,0 @@
-# hook to make a call to process rendered documents,
-Jekyll::Hooks.register :site, :post_render do |site|
-  site.collections.each do |_, collection|
-    site.process_documents(collection.docs)
-  end
-  site.process_documents(site.pages)
-end
